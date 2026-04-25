e7eb61ff98aa11997ac6c3bb8d31a45e6d9d9758
yash <yashsanap712@gmail.com>
2026-04-26 00:47:25 +0530

    base64 fixed

--- firebase.js
+++ firebase.js
@@ -1,6 +1,14 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || JSON.stringify(require('./serviceAccountKey.json')));
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+  serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8')
+  );
+} else {
+  serviceAccount = require('./serviceAccountKey.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
